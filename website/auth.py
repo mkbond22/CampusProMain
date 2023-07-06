0078ac6606385586e606e9c3cd2e7aa200031d48
MeganB <mbond1588@gmail.com>
2023-07-05 21:29:35 -0500

    additional pages/templates added
    Co-Authored-By: Tyler Selby <106200743+tselby81@users.noreply.github.com>

--- website/auth.py
+++ website/auth.py
@@ -64,3 +64,22 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
+
+@auth.route('/admin')
+def admin():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("admin.html", user=current_user)
